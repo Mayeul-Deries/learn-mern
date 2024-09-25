e10e290bfba40df4ca41a37f2b251c0f383604db
Mayeul Deries <mayeul.deries@gmail.com>
2024-09-25 11:17:42 +0200

    add delete and update methods

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,11 +1,80 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
-import { Box, Image, Heading, Text, HStack, IconButton, useColorModeValue,  } from '@chakra-ui/react'
-import React from 'react'
+import {
+  Box,
+  Image,
+  Heading,
+  Text,
+  HStack,
+  IconButton,
+  useColorModeValue,
+  useToast,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalHeader,
+  ModalContent,
+  ModalCloseButton,
+  VStack,
+  ModalBody,
+  Input,
+  ModalFooter,
+  Button,
+} from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useProductStore } from '../store/product'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const ProductCard = ({ product }: { product: any }) => {
-    const textColor = useColorModeValue('gray.600', 'gray.200')
-    const bg = useColorModeValue('white', 'gray.800')
+  const textColor = useColorModeValue('gray.600', 'gray.200')
+  const bg = useColorModeValue('white', 'gray.800')
+
+  const [updatedProduct, setUpdatedProduct] = useState(product)
+  const { updateProduct, deleteProduct } = useProductStore()
+  const toast = useToast()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  const handleUpdateProduct = async (pid: string, updatedProduct: any) => {
+    const {success, message } = await updateProduct(pid, updatedProduct)
+    onClose()
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    } else {
+      toast({
+        title: 'Success',
+        description: message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  }
+
+  const handleDeleteProduct = async (pid: string) => {
+    const { success, message } = await deleteProduct(pid)
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    } else {
+      toast({
+        title: 'Success',
+        description: message,
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  }
   return (
     <Box
       shadow={'lg'}
@@ -25,15 +94,71 @@ const ProductCard = ({ product }: { product: any }) => {
           {product.name}
         </Heading>
 
-        <Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>
+        <Text fontWeight='bold' fontSize='xl' color={textColor} mb={2}>
           ${product.price}
         </Text>
 
+        <Text fontSize='md' color={textColor} mb={4}>
+          {product.description}
+        </Text>
+
         <HStack spacing={2}>
-          <IconButton icon={<EditIcon />} colorScheme='blue' aria-label={'edit'} />
-          <IconButton icon={<DeleteIcon />} colorScheme='red' aria-label={'delete'} />
+          <IconButton icon={<EditIcon />} onClick={onOpen} colorScheme='blue' aria-label={'edit'} />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product?._id)}
+            colorScheme='red'
+            aria-label={'delete'}
+          />
         </HStack>
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+
+        <ModalContent>
+          <ModalHeader>Update Product</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={4}>
+              <Input
+                placeholder='Product Name'
+                name='name'
+                value={updatedProduct.name}
+                onChange={e => setUpdatedProduct({ ...updatedProduct, name: e.target.value })}
+              />
+              <Input
+                placeholder='Price'
+                name='price'
+                type='number'
+                value={updatedProduct.price}
+                onChange={e => setUpdatedProduct({ ...updatedProduct, price: e.target.value })}
+              />
+              <Input
+                placeholder='Description'
+                name='description'
+                value={updatedProduct.description}
+                onChange={e => setUpdatedProduct({ ...updatedProduct, description: e.target.value })}
+              />
+              <Input
+                placeholder='Image URL'
+                name='image'
+                value={updatedProduct.image}
+                onChange={e => setUpdatedProduct({ ...updatedProduct, image: e.target.value })}
+              />
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={() => handleUpdateProduct(product?._id, updatedProduct)}>
+              Update
+            </Button>
+            <Button variant='ghost' onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   )
 }
--- frontend/src/store/product.ts
+++ frontend/src/store/product.ts
@@ -13,7 +13,8 @@ interface ProductStore {
   setProducts: (products: Product[]) => void
   createProduct: (newProduct: Product) => Promise<{ success: boolean; message: string }>
   fetchProducts: () => Promise<void>
-  
+  deleteProduct: (pid: string) => Promise<{ success: boolean; message: string }>
+  updateProduct: (pid: string, updatedProduct: Product) => Promise<{ success: boolean; message: string }>
 }
 
 export const useProductStore = create<ProductStore>(set => ({
@@ -39,5 +40,38 @@ export const useProductStore = create<ProductStore>(set => ({
     const data = await response.json()
     set({ products: data.data })
     console.log('data', data)
-  }
+  },
+  deleteProduct: async (pid: string) => {
+    const response = await fetch(`/api/products/${pid}`, {
+      method: 'DELETE',
+    })
+    const data = await response.json()
+    if (!data.success) {
+      return { success: false, message: data.message }
+    }
+    // udpdate the ui immediately
+    set(state => ({ products: state.products.filter(product => product._id !== pid) }))
+    return { success: true, message: data.message }
+  },
+  updateProduct: async (pid: string, updatedProduct: Product) => {
+    if (!updatedProduct.name || !updatedProduct.price || !updatedProduct.description || !updatedProduct.image) {
+      return { success: false, message: 'All fields are required' }
+    }
+    const response = await fetch(`/api/products/${pid}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedProduct),
+    })
+    const data = await response.json()
+    if (!data.success) {
+      return { success: false, message: data.message }
+    }
+    // udpdate the ui immediately
+    set(state => ({
+      products: state.products.map(product => (product._id === pid ? data.data : product)),
+    }))
+    return { success: true, message: data.message }
+  },
 }))
